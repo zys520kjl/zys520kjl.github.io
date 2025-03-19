464dd733959db60b37ca0e9428382f72b9d0dc4d
zys520kjl <2376492728@qq.com>
2025-03-19 23:14:09 +0800

    Site updated: 2025-03-19 23:14:09

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/19/hello-world/"];function toRandomPost(){
+var posts=["2025/03/19/hello-world/","2025/03/19/测试文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
